28bb693deb0f87b3cfe2e660c0a2070e0e248349
Alvaro Gonzalez B <alvgonzalezb@gmail.com>
2023-05-20 18:33:32 -0600

    Dando estilos a las imágenes de la API

--- src/App.js
+++ src/App.js
@@ -25,16 +25,16 @@ function App() {
         <input type='text' placeholder='Find a Character'></input>
         <img className='logo-marvel' src='https://scontent.fmga3-2.fna.fbcdn.net/v/t1.18169-9/998558_604967956233177_396054649_n.png?_nc_cat=111&ccb=1-7&_nc_sid=174925&_nc_ohc=UQ22tm9r_48AX8knlre&_nc_ht=scontent.fmga3-2.fna&oh=00_AfCEqcslXAE8AeAW5HRmo8atY2A2htdZf_xOMbgr4N-9Tw&oe=6490C824' alt='logo-marvel'></img>
       </header>
-      <div>
+      <main>
         {characters.map(character => (
           <div>
-            <img key={character.id} src={`${character.thumbnail.path}.${character.thumbnail.extension}`} alt=''></img>
+            <img className='character-image' key={character.id} src={`${character.thumbnail.path}.${character.thumbnail.extension}`} alt=''></img>
             <div>
-              <p>{character.name}</p>
+              <p className='character-name'>{character.name}</p>
             </div>
           </div>
         ))}
-      </div>
+      </main>
     </div>
   );
 }
